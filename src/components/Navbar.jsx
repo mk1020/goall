47c483a9a11af8d72b90efd7fa28f9cfc5746057
Misha <mk256@localhost.localdomain>
2020-01-10 20:21:07 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a>Profile</a>
+      </div>
+
+      <div>
+        <a>dialogs</a>
+      </div>
+
+      <div>
+        <a>goals</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
