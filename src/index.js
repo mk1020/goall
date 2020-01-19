8acff9e3d574f2414b545c08952b4f47023fa77c
Mikhail <mk256@localhost.localdomain>
2020-01-20 00:07:33 +0200

    --

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, newTextPost, subscribe} from './redux/state'
 
 
- let rerenderEntireTree = ()=>{
-    ReactDOM.render(<App state={state} addPost={addPost} newTextPost={newTextPost}/>, document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(<App state={state} addPost={store.addPost.bind(store)} newTextPost={store.newTextPost.bind(store)} />, document.getElementById('root'));
 
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
-subscribe(rerenderEntireTree);
 serviceWorker.unregister();
--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,39 +1,44 @@
-let state = {
-  dialogsPage: {
-    dialogsData: [
-      { id: 1, name: "Masha" },
-      { id: 2, name: "Mike" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Will" }
-    ],
+let store = {
+  _state: {
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: "Masha" },
+        { id: 2, name: "Mike" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Will" }
+      ],
 
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "Hello" },
-      { id: 3, message: "How are you" },
-      { id: 4, message: "How are you feeling?" }
-    ]
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "Hello" },
+        { id: 3, message: "How are you" },
+        { id: 4, message: "How are you feeling?" }
+      ]
+    },
+
+    profilePage: {
+      MyPostData: [{ post: "Text Text Text Text Text Text ", likes: 15 }],
+      TextPost: "132"
+    }
   },
 
-  profilePage: {
-    MyPostData: [{ post: "Text Text Text Text Text Text ", likes: 15 }],
-    TextPost: '132'
+  getState() {
+    return this._state;
+  },
+
+  addPost(postMessage) {
+    this._state.profilePage.MyPostData.push(postMessage);
+    this._state.profilePage.TextPost = "";
+    this.rerenderEntireTree(this._state);
+  },
+
+  newTextPost(newText) {
+    this._state.profilePage.TextPost = newText;
+    this.rerenderEntireTree(this._state);
+  },
+
+  subscribe(observer) {
+    this.rerenderEntireTree = observer;
   }
 };
-
-let rerenderEntireTree;
-export const addPost = (postMessage) => {
-   state.profilePage.MyPostData.push(postMessage);
-   state.profilePage.TextPost='';
-   rerenderEntireTree(state);
-}
-
-export const newTextPost = (newText) => {
-  state.profilePage.TextPost=newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-export default state;
+export default store;
